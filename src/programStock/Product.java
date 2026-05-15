dc8846bda6f57f04eafdc123d6afdfdcb113d3da
William Bós <boswilliam56@gmail.com>
2026-05-15 10:39:03 -0300

    atualizacao de programStock adicionando contrutor no product

--- src/programStock/Product.java
+++ src/programStock/Product.java
@@ -5,9 +5,17 @@ public class Product {
     public double price;
     public int quantity;
 
+    public Product (String name, double price, int quantity){
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+
+    }
+
     public double totalValueInStock(){
         return price * quantity;
     }
+
     public void addProducts(int quantity){
         this.quantity += quantity;
 
